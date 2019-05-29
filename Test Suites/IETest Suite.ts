db9b223f08589e60f2cc82c79a0f80a1bc524fbc
Admin <Admin@DESKTOP-HBA76UR>
2019-05-29 20:20:55 +0300

    02 commit

---
+++ Test Suites/IETest Suite.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>IETest Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e195d6c6-d464-4872-b2b3-ba442957ec60</testSuiteGuid>
+   <testCaseLink>
+      <guid>bc7db814-b363-49dd-adf8-6aa186a8736a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IE_01_01_account_google_empty_mail_db</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>688bc902-3476-4850-9d72-bb1012783cc8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IE_01_02_accounts_google_nonexistent_mail_db</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a4f2dedf-cb0a-4c09-8219-17ef3209f38c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IE_01_03_accounts_google_existent_mail_db</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>17b50321-9878-4ac2-8455-32dcfc7b6868</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IE_02_01_account_google_empty_login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2928f823-366e-4467-8a0b-42947e7c7887</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IE_02_02_account_google_wrong_login</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7da0e48e-ead5-43c4-8051-ff368fc93f4e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IE_02_03_accounts_google_existent_login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
